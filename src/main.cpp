4d49063de96a3f149899185a6e6a0a57fdc41f86
stevenpaul007-creator <stevenpaul.007@gmail.com>
2025-12-05 17:57:40 +0800

    support 9329

--- src/main.cpp
+++ src/main.cpp
@@ -55,12 +55,41 @@ SOFTWARE.
 // in bootloader upload mode before using the IDE to upload.
 // WARNING: The program stops working if USB_DEBUG is set to 0. Leave it on
 // until this is solved.
+
+#include <Arduino.h>
+#include "CH9329.h"
+CH9329 *CH9329Client;
+
+#include "FreeRTOS.h"           // 包含 FreeRTOS 头文件
+#include "semphr.h"             // 包含信号量和互斥锁相关的头文件
+SemaphoreHandle_t ch9329_mutex; // 声明一个互斥锁句柄
+
+struct CH9329CFG ch9329cfgs[CH9329COUNT] = {
+    {.led_pin = 2,
+     .rx_pin = 8,
+     .tx_pin = 9,
+     .cfg1_pin = 10,
+     .mode1_pin = 11,
+     .addr = 0x30,
+     .baud = 9600,
+     .CFG1 = HIGH,
+     .MODE1 = HIGH},
+    {.led_pin = 28,
+     .rx_pin = 8,
+     .tx_pin = 9,
+     .cfg1_pin = 27,
+     .mode1_pin = 26,
+     .addr = 0x01,
+     .baud = 9600,
+     .CFG1 = HIGH,
+     .MODE1 = HIGH}};
+
 #define USB_DEBUG 1
 
 #if USB_DEBUG
-#define DBG_print(...)    Serial.print(__VA_ARGS__)
-#define DBG_println(...)  Serial.println(__VA_ARGS__)
-#define DBG_printf(...)   Serial.printf(__VA_ARGS__)
+#define DBG_print(...) Serial.print(__VA_ARGS__)
+#define DBG_println(...) Serial.println(__VA_ARGS__)
+#define DBG_printf(...) Serial.printf(__VA_ARGS__)
 #else
 #define DBG_print(...)
 #define DBG_println(...)
@@ -75,16 +104,131 @@ SOFTWARE.
 // USB Host object
 Adafruit_USBH_Host USBHost;
 
-// HID report descriptor using TinyUSB's template
-// Single Report (no ID) descriptor
-uint8_t const desc_hid_report[] =
-{
-  TUD_HID_REPORT_DESC_MOUSE()
-};
+bool Skip_report_id = false;
 
-// USB HID object. For ESP32 these values cannot be changed after this declaration
-// desc report, desc len, protocol, interval, use out endpoint
-Adafruit_USBD_HID usb_hid(desc_hid_report, sizeof(desc_hid_report), HID_ITF_PROTOCOL_MOUSE, 2, false);
+// Invoked when device with hid interface is mounted
+// Report descriptor is also available for use.
+// tuh_hid_parse_report_descriptor() can be used to parse common/simple enough
+// descriptor. Note: if report descriptor length > CFG_TUH_ENUMERATION_BUFSIZE,
+// it will be skipped therefore report_desc = NULL, desc_len = 0
+void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const *desc_report, uint16_t desc_len)
+{
+  (void)desc_report;
+  (void)desc_len;
+  uint16_t vid, pid;
+  tuh_vid_pid_get(dev_addr, &vid, &pid);
+
+  DBG_printf("HID device address = %d, instance = %d is mounted\r\n", dev_addr, instance);
+  DBG_printf("VID = %04x, PID = %04x\r\n", vid, pid);
+  uint8_t const protocol_mode = tuh_hid_get_protocol(dev_addr, instance);
+  uint8_t const itf_protocol = tuh_hid_interface_protocol(dev_addr, instance);
+  DBG_printf("protocol_mode=%d,itf_protocol=%d\r\n",
+             protocol_mode, itf_protocol);
+
+  const size_t REPORT_INFO_MAX = 8;
+  tuh_hid_report_info_t report_info[REPORT_INFO_MAX];
+  uint8_t report_num = tuh_hid_parse_report_descriptor(report_info,
+                                                       REPORT_INFO_MAX, desc_report, desc_len);
+  DBG_printf("HID descriptor reports:%d\r\n", report_num);
+  bool hid_mouse = false;
+  for (size_t i = 0; i < report_num; i++)
+  {
+    DBG_printf("%d,%d,%d\r\n", report_info[i].report_id, report_info[i].usage,
+               report_info[i].usage_page);
+    Skip_report_id = false;
+    if ((report_info[i].usage_page == 1) && (report_info[i].usage == 2))
+    {
+      hid_mouse = true;
+      if (itf_protocol == HID_ITF_PROTOCOL_NONE)
+        Skip_report_id = report_info[i].report_id != 0;
+      else if (protocol_mode == HID_PROTOCOL_BOOT)
+        Skip_report_id = false;
+      else
+        Skip_report_id = report_info[i].report_id != 0;
+      break;
+    }
+  }
+
+  if (desc_report && desc_len)
+  {
+    for (size_t i = 0; i < desc_len; i++)
+    {
+      DBG_printf("%x,", desc_report[i]);
+    }
+    DBG_println();
+  }
+
+  if ((itf_protocol == HID_ITF_PROTOCOL_MOUSE) || hid_mouse)
+  {
+    DBG_println("HID Mouse");
+    if (!tuh_hid_receive_report(dev_addr, instance))
+    {
+      DBG_println("Error: cannot request to receive report");
+    }
+  }
+}
+
+// Invoked when device with hid interface is un-mounted
+void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance)
+{
+  DBG_printf("HID device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
+}
+
+// Invoked when received report from device via interrupt endpoint
+void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance,
+                                uint8_t const *report, uint16_t len)
+{
+  if (Skip_report_id)
+  {
+    // Skip first byte which is report ID.
+    report++;
+    len--;
+  }
+  if (len > 2)
+  {
+    hid_mouse_report_t mouseRpt = {0};
+    mouseRpt.buttons = report[0];
+    mouseRpt.x = report[1];
+    mouseRpt.y = report[2];
+    if (len > 3)
+    {
+      mouseRpt.wheel = report[3];
+      if (len > 4)
+      {
+        mouseRpt.pan = report[4];
+      }
+    }
+    // DBG_printf("mouse moved to x=%d, y=%d\r\n", mouseRpt.x, mouseRpt.y);
+
+    // 使用互斥锁保护 mouseMove 调用
+    if (xSemaphoreTake(ch9329_mutex, pdMS_TO_TICKS(10)) == pdTRUE)
+    { // 设置10ms超时
+      for (int i = 0; i < CH9329COUNT; i++)
+      {
+        if (CH9329Client->isUSBConnected(i))
+        {
+          CH9329Client->mouseMove(i, mouseRpt.x, mouseRpt.y, mouseRpt.buttons, mouseRpt.wheel);
+        }
+      }
+      xSemaphoreGive(ch9329_mutex);
+    }
+    else
+    {
+      // DBG_println("Warning: Could not get mutex in callback!");
+    }
+  }
+  else
+  {
+    static uint32_t drops = 0;
+    DBG_printf("drops=%lu\r\n", ++drops);
+  }
+
+  // continue to request to receive report
+  if (!tuh_hid_receive_report(dev_addr, instance))
+  {
+    DBG_println("Error: cannot request to receive report");
+  }
+}
 
 //--------------------------------------------------------------------+
 // Setup and Loop on Core0
@@ -94,43 +238,81 @@ void setup()
 {
 #if USB_DEBUG
   Serial.begin(115200);
+  DBG_println("Serial inited.");
 #else
   Serial.end();
 #endif
-  usb_hid.begin();
 
+  // 创建一个互斥信号量
+  ch9329_mutex = xSemaphoreCreateMutex();
+  if (ch9329_mutex == NULL)
+  {
+    // 互斥锁创建失败，可能内存不足或系统错误
+    DBG_println("Error: Failed to create CH9329 mutex!");
+    while (1)
+      ; // 停止程序
+  }
+
+  CH9329Client = new CH9329(&Serial2, ch9329cfgs);
+
+  // 获取锁
+  if (xSemaphoreTake(ch9329_mutex, portMAX_DELAY) == pdTRUE)
+  {
+    for (int i = 0; i < CH9329COUNT; i++)
+    {
+      CH9329Client->cmdReset(i);
+      delay(10);
+      CH9329Client->cmdGetInfo(i);
+      delay(10);
+      DBG_printf("CH9329[%d] inited.\r\n", i);
+    }
+    // 释放锁
+    xSemaphoreGive(ch9329_mutex);
+  }
+
+  // CH9329Client->readUart();
   // wait until device mounted
-  while( !USBDevice.mounted() ) delay(1);
-
-#if USB_DEBUG
-  while ( !Serial ) delay(10);   // wait for native usb
-#endif
+  while (!USBDevice.mounted())
+    delay(1);
   DBG_println("USB Boot Mouse pass through");
 }
 
 void loop()
 {
+
+  // 尝试获取互斥锁，等待无限长时间
+  if (xSemaphoreTake(ch9329_mutex, portMAX_DELAY) == pdTRUE)
+  {
+    // 成功获取锁，可以安全地访问 CH9329Client
+    CH9329Client->readUart();
+
+    // 操作完成后，必须释放互斥锁
+    xSemaphoreGive(ch9329_mutex);
+  }
+  delay(10);
 }
 
 //--------------------------------------------------------------------+
 // Setup and Loop on Core1
 //--------------------------------------------------------------------+
 
-void setup1() {
-#if USB_DEBUG
-  while ( !Serial ) delay(10);   // wait for native usb
-#endif
+void setup1()
+{
+  delay(3000);
   DBG_println("Core1 setup to run TinyUSB host with pio-usb");
 
   // Check for CPU frequency, must be multiple of 120Mhz for bit-banging USB
   uint32_t cpu_hz = clock_get_hz(clk_sys);
-  if ( cpu_hz != 120000000UL && cpu_hz != 240000000UL ) {
+  if (cpu_hz != 120000000UL && cpu_hz != 240000000UL)
+  {
 #if USB_DEBUG
-    while ( !Serial ) delay(10);   // wait for native usb
+    while (!Serial)
+      delay(10); // wait for native usb
 #endif
     DBG_printf("Error: CPU Clock = %lu, PIO USB require CPU clock must be multiple of 120 Mhz\r\n", cpu_hz);
     DBG_println("Change your CPU Clock to either 120 or 240 Mhz in Menu->CPU Speed");
-    while(1) delay(1);
+    while (1)
+      delay(1);
   }
 
 #ifdef PIN_PIO_USB_HOST_VBUSEN
@@ -151,98 +333,4 @@ void setup1() {
 void loop1()
 {
   USBHost.task();
-}
-
-bool Skip_report_id = false;
-
-// Invoked when device with hid interface is mounted
-// Report descriptor is also available for use.
-// tuh_hid_parse_report_descriptor() can be used to parse common/simple enough
-// descriptor. Note: if report descriptor length > CFG_TUH_ENUMERATION_BUFSIZE,
-// it will be skipped therefore report_desc = NULL, desc_len = 0
-void tuh_hid_mount_cb(uint8_t dev_addr, uint8_t instance, uint8_t const *desc_report, uint16_t desc_len) {
-  (void) desc_report;
-  (void) desc_len;
-  uint16_t vid, pid;
-  tuh_vid_pid_get(dev_addr, &vid, &pid);
-
-  DBG_printf("HID device address = %d, instance = %d is mounted\r\n", dev_addr, instance);
-  DBG_printf("VID = %04x, PID = %04x\r\n", vid, pid);
-  uint8_t const protocol_mode = tuh_hid_get_protocol(dev_addr, instance);
-  uint8_t const itf_protocol = tuh_hid_interface_protocol(dev_addr, instance);
-  DBG_printf("protocol_mode=%d,itf_protocol=%d\r\n",
-      protocol_mode, itf_protocol);
-
-  const size_t REPORT_INFO_MAX = 8;
-  tuh_hid_report_info_t report_info[REPORT_INFO_MAX];
-  uint8_t report_num = tuh_hid_parse_report_descriptor(report_info,
-      REPORT_INFO_MAX, desc_report, desc_len);
-  DBG_printf("HID descriptor reports:%d\r\n", report_num);
-  bool hid_mouse = false;
-  for (size_t i = 0; i < report_num; i++) {
-    DBG_printf("%d,%d,%d\r\n", report_info[i].report_id, report_info[i].usage,
-        report_info[i].usage_page);
-    Skip_report_id = false;
-    if ((report_info[i].usage_page == 1) && (report_info[i].usage == 2)) {
-      hid_mouse = true;
-      if (itf_protocol == HID_ITF_PROTOCOL_NONE)
-        Skip_report_id = report_info[i].report_id != 0;
-      else if (protocol_mode == HID_PROTOCOL_BOOT)
-        Skip_report_id = false;
-      else
-        Skip_report_id = report_info[i].report_id != 0;
-      break;
-    }
-  }
-
-  if (desc_report && desc_len) {
-    for (size_t i = 0; i < desc_len; i++) {
-      DBG_printf("%x,", desc_report[i]);
-    }
-    DBG_println();
-  }
-
-  if ((itf_protocol == HID_ITF_PROTOCOL_MOUSE) || hid_mouse) {
-    DBG_println("HID Mouse");
-    if (!tuh_hid_receive_report(dev_addr, instance)) {
-      DBG_println("Error: cannot request to receive report");
-    }
-  }
-}
-
-// Invoked when device with hid interface is un-mounted
-void tuh_hid_umount_cb(uint8_t dev_addr, uint8_t instance) {
-  DBG_printf("HID device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
-}
-
-// Invoked when received report from device via interrupt endpoint
-void tuh_hid_report_received_cb(uint8_t dev_addr, uint8_t instance,
-    uint8_t const *report, uint16_t len) {
-  if (Skip_report_id) {
-    // Skip first byte which is report ID.
-    report++;
-    len--;
-  }
-  if (usb_hid.ready() && (len > 2)) {
-    hid_mouse_report_t mouseRpt = {0};
-    mouseRpt.buttons = report[0];
-    mouseRpt.x = report[1];
-    mouseRpt.y = report[2];
-    if (len > 3) {
-      mouseRpt.wheel = report[3];
-      if (len > 4) {
-        mouseRpt.pan = report[4];
-      }
-    }
-    usb_hid.sendReport(0, (void *)&mouseRpt, sizeof(mouseRpt));
-  }
-  else {
-    static uint32_t drops = 0;
-    DBG_printf("drops=%lu\r\n", ++drops);
-  }
-
-  // continue to request to receive report
-  if (!tuh_hid_receive_report(dev_addr, instance)) {
-    DBG_println("Error: cannot request to receive report");
-  }
 }
